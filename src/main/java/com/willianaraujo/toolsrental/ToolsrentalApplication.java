79e64c81922834d2c87f53d2740b9259c8399087
Willian Araujo <willianaraujoptc@gmail.com>
2023-03-31 19:30:01 -0300

    primeiro commit

---
+++ src/main/java/com/willianaraujo/toolsrental/ToolsrentalApplication.java
@@ -0,0 +1,13 @@
+package com.willianaraujo.toolsrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsrentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willianaraujo/toolsrental/ToolsrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willianaraujo.toolsrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
