b8bdb2d109796cf62cf7f82c5951ff191bcce8d6
Willian Araujo <willianaraujoptc@gmail.com>
2023-04-04 19:30:42 -0300

    Correção Controller adress faltou rota

--- src/main/java/com/willianaraujo/toolsrental/controller/AddressesController.java
+++ src/main/java/com/willianaraujo/toolsrental/controller/AddressesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("api/v1/addresses")
 public class AddressesController {
     private final AddressRepository addressRepository;
 
